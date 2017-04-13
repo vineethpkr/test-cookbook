64e4753ac545dab8330de0357856e8564b1f1a2d
Vineeth <vineeth@freshdesk.com>
2017-04-13 15:50:28 +0530

    Flask test recipe

--- git-cookbook/recipes/default.rb
+++ git-cookbook/recipes/default.rb
@@ -1,7 +1,20 @@
-if node['platform'] == "ubuntu" && node['platform_version'].to_f <= 10.04
-  package "git-core"
-else
-  package "git"
-end
+# if node['platform'] == "ubuntu" && node['platform_version'].to_f <= 10.04
+#   package "git-core"
+# else
+#   package "git"
+# end
 
-log "Well, that was too easy"
+# log "Well, that was too easy"
+
+#
+# Cookbook Name:: first_test
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+include_recipe 'poise-python'
+include_recipe 'apt::default'
+python_runtime '2'
+application '/srv/flask_test' do
+	git 'https://github.com/poise/test_flask.git'
+	pip_requirements
+end
