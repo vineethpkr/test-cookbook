7670f3779317087e96d85586842b0bf1214430c3
Vineeth <vineeth@freshdesk.com>
2017-04-13 16:52:51 +0530

    Adding default_1

---
+++ git-cookbook/recipes/default_1.rb
@@ -0,0 +1,25 @@
+# if node['platform'] == "ubuntu" && node['platform_version'].to_f <= 10.04
+#   package "git-core"
+# else
+#   package "git"
+# end
+
+# log "Well, that was too easy"
+
+#
+# Cookbook Name:: first_test
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+include_recipe 'poise-python'
+include_recipe 'apt::default'
+python_runtime '2'
+application '/srv/flask_test_2' do
+	git 'https://github.com/vineethpkr/test_flask.git'
+	pip_requirements
+	gunicorn do
+    	port 5000
+  	end
+end
+
+
