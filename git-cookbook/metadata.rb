f1bb72510667be722521dccbe5f1a68e2807c86f
Vineeth <vineeth@freshdesk.com>
2017-04-13 15:10:58 +0530

    Addition to metadata

--- git-cookbook/metadata.rb
+++ git-cookbook/metadata.rb
@@ -1,4 +1,13 @@
 name "git"
 version "2.10.1"
 
-depends "application"
+depends 'application', '~> 5.0'
+depends 'application_git', '~> 1.0'
+# depends 'application_javascript', '~> 1.0'
+depends 'application_python', '~> 4.0'
+# depends ‘application_ruby’, ‘~> 4.0’
+# depends ‘build-essential’
+depends 'poise', '~> 2.0'
+depends 'poise-python'
+depends 'poise-archive', '~> 1.3'
+depends 'apt', '~> 6.0.1'
