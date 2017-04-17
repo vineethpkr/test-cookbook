efa3c12fdd9b380a96fbdfb5f274c8914bdc4735
Sarthak Dev <sarthakdev90@gmail.com>
2017-04-17 17:36:50 +0530

    Removing supervisord config from file

--- git-cookbook/recipes/settings_supervisor.rb
+++ git-cookbook/recipes/settings_supervisor.rb
@@ -1,7 +1,7 @@
-execute "get_supervisor" do
-  command "sudo easy_install supervisor"
-  command "echo_supervisord_conf > /etc/supervisord.conf"
-end
+# execute "get_supervisor" do
+#   command "sudo easy_install supervisor"
+#   #command "echo_supervisord_conf > /etc/supervisord.conf"
+# end
 
 supervisord_program "app" do
   command "/.virtualenvs/settings/bin/gunicorn manage:app --bind 0.0.0.0:8000"
