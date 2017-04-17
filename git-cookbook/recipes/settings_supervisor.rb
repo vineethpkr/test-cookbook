82d086b7e2f55ed321a799286c0f3b3f64c03831
Sarthak Dev <sarthakdev90@gmail.com>
2017-04-17 17:29:13 +0530

    Add supervisor

--- git-cookbook/recipes/settings_supervisor.rb
+++ git-cookbook/recipes/settings_supervisor.rb
@@ -1,3 +1,8 @@
+execute "get_supervisor" do
+  command "sudo easy_install supervisor"
+  command "echo_supervisord_conf > /etc/supervisord.conf"
+end
+
 supervisord_program "app" do
   command "/.virtualenvs/settings/bin/gunicorn manage:app --bind 0.0.0.0:8000"
   directory '/srv/settings/'
