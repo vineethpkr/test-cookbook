68e1dc562fa7c0140ecc8d31d056b4e3b5a1f1b9
Sarthak Dev <sarthakdev90@gmail.com>
2017-04-17 17:10:39 +0530

    Supervisor file fix

--- git-cookbook/recipes/settings_supervisor.rb
+++ git-cookbook/recipes/settings_supervisor.rb
@@ -1,5 +1,5 @@
 supervisord_program "app" do
-  command "/.virtualenvs/settings/bin/gunicorn main:app --bind 0.0.0.0:8000"
+  command "/.virtualenvs/settings/bin/gunicorn manage:app --bind 0.0.0.0:8000"
   directory '/srv/settings/'
   autostart true
   autorestart true
