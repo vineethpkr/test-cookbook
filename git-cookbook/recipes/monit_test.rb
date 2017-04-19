15bdbdd312bc2b98e219e20d22793e29468d333c
Sarthak Dev <sarthakdev90@gmail.com>
2017-04-19 11:28:39 +0530

    Testing monit

---
+++ git-cookbook/recipes/monit_test.rb
@@ -0,0 +1,10 @@
+python_runtime '2'
+
+poise_service 'flask_check' do
+  command '/.virtualenvs/settings/bin/gunicorn main:app --bind 127.0.0.1:5090'
+  directory '/srv/flask_test/test_flask'
+  stop_signal 'WINCH'
+  reload_signal 'USR1'
+  provider :monit
+  #options :monit, checks: 'if failed host localhost port 80 protocol HTTP request "/" then restart'
+end
