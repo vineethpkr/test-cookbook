afe52c19cfe2fc2574ecb0dbaeb4dd807121666b
Sarthak Dev <sarthakdev90@gmail.com>
2017-04-25 15:42:33 +0530

    Adding supervisor commit

---
+++ supervisor_web/recipes/install.rb
@@ -0,0 +1,14 @@
+python_runtime '2'
+#python_virtualenv '/.virtualenvs/settings/'
+
+
+Chef::Log.info 'installing supervisor'
+
+execute 'install supervisor' do
+    command 'source ~/.virtualenvs/social_tab_3/bin/activate && pip install supervisor && pip install superlance'
+end
+
+
+execute 'create conf file' do
+    command 'touch /var/tmp/supervisor/supervisord.conf'
+end
