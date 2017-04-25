6275fbb1e03cb6d1b38dcf350e1178b3cc6a5517
Sarthak Dev <sarthakdev90@gmail.com>
2017-04-25 18:17:17 +0530

    Adding setup recipes for Subscription Manager

--- subscription_manager/recipes/setup.rb
+++ subscription_manager/recipes/setup.rb
@@ -5,9 +5,9 @@ include_recipe 'apt::default'
 # include_recipe 'gunicorn'
 
 python_runtime '2'
-python_virtualenv '/.virtualenvs/settings/'
+python_virtualenv '/.virtualenvs/subscription_manager/'
 
-directory '/.virtualenvs/settings/' do
+directory '/.virtualenvs/subscription_manager/' do
 	mode '777'
 	action :create
 end
@@ -16,17 +16,11 @@ end
 #   version '1.8'
 # end
 
-application '/srv/settings' do
+application '/srv/subscription_manager' do
   #virtualenv
-  git "#{node[:settings][:repository_name]}" do
+  git "#{node[:subscription_manager][:repository_name]}" do
     deploy_key "#{node[:settings][:deploy_key]}"
-    revision "#{node[:settings][:revision]}"
+    revision "#{node[:subscription_manager][:revision]}"
   end
   pip_requirements
 end
-
-# # Start
-# execute 'guni' do
-# 	cwd '/srv/settings'
-# 	command '/.virtualenvs/settings/bin/gunicorn manage:app --bind 0.0.0.0:8000 --daemon'
-# end
