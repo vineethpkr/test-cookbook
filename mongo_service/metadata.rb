ec39744e49d3fc6dea9261eaadd13e8529776a4a
Sarthak Dev <sarthakdev90@gmail.com>
2017-05-02 13:51:24 +0530

    Quote fix

--- mongo_service/metadata.rb
+++ mongo_service/metadata.rb
@@ -12,7 +12,7 @@ depends 'application_git', '~> 1.0'
 # depends 'application_javascript', '~> 1.0'
 depends 'application_python', '~> 4.0'
 # depends ‘application_ruby’, ‘~> 4.0’
-depends ‘build-essential’
+depends 'build-essential'
 depends 'poise', '~> 2.0'
 depends 'poise-python'
 depends 'poise-archive', '~> 1.3'
